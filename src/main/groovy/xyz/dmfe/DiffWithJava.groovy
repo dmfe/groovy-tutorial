140f009bc37f9e02324c63d35f23f90754ac2a97
Dmitry Fedotov <dm.fe@yandex.ru>
2022-02-28 23:19:27 +0300

    Differences with Java. Array Initializers.

--- src/main/groovy/xyz/dmfe/DiffWithJava.groovy
+++ src/main/groovy/xyz/dmfe/DiffWithJava.groovy
@@ -1,11 +1,18 @@
 package xyz.dmfe
 
 class DiffWithJava {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         Object o = "Object"
         int result = method(o)
 
         println result
+
+        int[] array = [1, 2, 3]
+
+        println array
+
+        // For Groovy 3+, you can optionally use the Java array initializers long syntax:
+        // def array2 = new int[] {1, 2, 3}
     }
 
     static int method(String arg) {
--- src/main/java/xyz/dmfe/DiffWithGroovy.java
+++ src/main/java/xyz/dmfe/DiffWithGroovy.java
@@ -6,6 +6,19 @@ public class DiffWithGroovy {
         int result = method(o);
 
         System.out.println(result);
+
+        int[] array = {1, 2, 3};
+        int[] array2 = new int[] {4, 5, 6};
+
+        System.out.println("Array one:");
+        for (int a : array) {
+            System.out.print(a + " ");
+        }
+
+        System.out.println("\nArray two:");
+        for (int a : array2) {
+            System.out.print(a + " ");
+        }
     }
 
     static int method(String arg) {
